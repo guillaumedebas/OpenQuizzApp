f5bf4365b2babf4287ee5ee5df917b5277aee189
Guillaume Debas <guillaumedebas@hotmail.fr>
2024-01-06 23:52:23 +0100

    checkAnswers() wip

--- src/assets/Components/Common/BasicQuiz/BasicQuiz.jsx
+++ src/assets/Components/Common/BasicQuiz/BasicQuiz.jsx
@@ -1,21 +1,36 @@
 import { Box } from '@mui/material'
 import BasicQuestion from '../BasicQuestion/BasicQuestion'
 import PropTypes from 'prop-types'
-import BasicButton from '../BasicButton/BasicButton' 
+import BasicButton from '../BasicButton/BasicButton'
+import { useState } from 'react'
+
 
 const BasicQuiz = ({ questions }) => {
- const handleAnswerChange = (questionId, selectedAnswer) => {
-  console.log(questionId, selectedAnswer)
-  
+  const [selectedAnswers, setSelectedAnswers] = useState({})
+
+  const handleAnswerChange = (questionId, selectedAnswer) => {
+    const updatedAnswers = { ...selectedAnswers }
+    updatedAnswers[questionId] = selectedAnswer
+    setSelectedAnswers(updatedAnswers)
+
+  };
+  const checkAnswers = () => {
+    for (const questionId in selectedAnswers) {
+      if (Object.hasOwnProperty.call(selectedAnswers, questionId)) {
+        const selectedAnswer = selectedAnswers[questionId];
+        console.log(`Question ID: ${questionId}, Selected Answer: ${selectedAnswer}`);
+      }
+    }
   };
 
+
   return (
-    <Box sx={{ 
-      display: 'flex', 
+    <Box sx={{
+      display: 'flex',
       flexDirection: 'column',
-      alignItems: 'center' 
-       }}>
-     {questions.map((question) => (
+      alignItems: 'center'
+    }}>
+      {questions.map((question) => (
         <BasicQuestion
           key={question.questionId}
           question={question}
@@ -26,9 +41,10 @@ const BasicQuiz = ({ questions }) => {
         variant="contained"
         color="primary"
         size="small"
-        sx={{ 
-            mt: 2,
+        sx={{
+          mt: 2,
         }}
+        onClick={checkAnswers}
       >
         Valider
       </BasicButton>
@@ -36,15 +52,15 @@ const BasicQuiz = ({ questions }) => {
   )
 }
 BasicQuiz.propTypes = {
-    questions: PropTypes.arrayOf(PropTypes.shape({
-        questionId: PropTypes.number.isRequired,
-        questionText: PropTypes.string.isRequired,
-        answers: PropTypes.arrayOf(PropTypes.shape({
-            answersId: PropTypes.number.isRequired,
-            label: PropTypes.string.isRequired,
-            correct: PropTypes.bool.isRequired
-        })).isRequired
+  questions: PropTypes.arrayOf(PropTypes.shape({
+    questionId: PropTypes.number.isRequired,
+    questionText: PropTypes.string.isRequired,
+    answers: PropTypes.arrayOf(PropTypes.shape({
+      answersId: PropTypes.number.isRequired,
+      label: PropTypes.string.isRequired,
+      correct: PropTypes.bool.isRequired
     })).isRequired
+  })).isRequired
 };
 
 export default BasicQuiz
